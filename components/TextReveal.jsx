05bae7720979f9e13f1b71461dc4846a18e2e78e
jaxo <jaroba0@gmail.com>
2024-12-30 13:38:20 +0100

    :)

--- components/TextReveal.jsx
+++ components/TextReveal.jsx
@@ -4,7 +4,7 @@ import TextReveal from "./Text-reveal";
 export function TextRevealDemo() {
   return (
     <div className="z-10 flex min-h-[16rem] items-center justify-center rounded-lg bg-[#080808] ">
-      <TextReveal text="Apollo11 changed us." />
+      <TextReveal text="Apollo 11 changed us." />
     </div>
   );
 }
